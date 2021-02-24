edd2e6eeb794301b004af767b2a2b9fcc32ec5f1
horacinis <horacinis@gmail.com>
2021-02-24 15:39:42 +1100

    Added Error message when song not found

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+import ErrorFound from './ErrorFound';
 
-const Lyrics = ({ lyrics, artist, song }) => {
+const Lyrics = ({ lyrics, artist, song, errorFound }) => {
 
     const [showMore, setShowMore] = useState(false);
+    if (errorFound) {
+        return (
+            <ErrorFound />
+        )
+    }
     return (
         <section className='text-center'>
-            {artist && <div>
+            {lyrics && <div>
                 <h2>{`${song.toUpperCase()} by ${artist.toUpperCase()}`}</h2>
                 <pre>{!showMore ? `${lyrics.lyrics.substring(0, 200)}...` : lyrics.lyrics}</pre>
                 <button onClick={() => setShowMore(!showMore)} className='btn btn-outline-dark' type='button'>
